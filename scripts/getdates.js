9e915f4eab9e337e1aee4a6b1b3d5e53d9b867fc
adepegba1 <adepegbadavid@rocketmail.com>
2025-09-05 12:28:26 +0100

    about page(HTML, CSS, JavaScript)

---
+++ scripts/getdates.js
@@ -0,0 +1,16 @@
+// This finds the HTML element with id="currentyear" and stores it in a variable
+const currentyear = document.querySelector("#currentyear");
+
+// Creates a new Date object containing the current date and time (from your computer or server).
+const now = new Date();
+
+// This writes text into the HTML element with id="currentyear"
+// This uses the Intl.DateTimeFormat API to format the date nicely.
+currentyear.innerHTML = `Last modification:
+${new Intl.DateTimeFormat("en-US", 
+    {
+        dateStyle:"medium",
+        timeStyle: "medium",
+        hour12: false
+    }
+).format(now)}`;
