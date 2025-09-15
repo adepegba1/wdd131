5c4be8fac1b35fa74d9034f1351c65896df7bcbd
adepegba1 <adepegbadavid@rocketmail.com>
2025-09-15 16:11:36 +0100

    responsive image'

--- scripts/place.js
+++ scripts/place.js
@@ -13,5 +13,23 @@ ${new Intl.DateTimeFormat("en-US", {
   hour12: false,
 }).format(now)}`;
 
-const temperature = 30;
-const wind = 15;
+// windwill function calculation
+const temperature = 8;
+const WindSpeed = 7;
+
+const calculateWindChill = (temp, speed) => {
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed, 0.16) +
+    0.3965 * temp * Math.pow(speed, 0.16)
+  );
+};
+const windChillElement = document.getElementById("windchill");
+
+if (temperature <= 10 && WindSpeed > 4.8) {
+  const WindChill = calculateWindChill(temperature, WindSpeed);
+  windChillElement.textContent = WindChill.toFixed(2) + "C";
+} else {
+  windChillElement.textContent = "N/A";
+}
