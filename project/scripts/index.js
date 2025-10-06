832ca0597378bca369c05d3841f0c5b5518ad576
adepegba1 <adepegbadavid@rocketmail.com>
2025-10-06 17:54:25 +0100

    homepage done

---
+++ project/scripts/index.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+document.getElementById(
+  "currentyear"
+).innerHTML = `&copy;${date.getFullYear()}  🌺Adepegba David🌺 Nigeria`;
+
+document.getElementById(
+  "last-modified"
+).textContent = `Last Modified ${document.lastModified}`;
