e7d2825c4fb17ab2815d22130f76347d80e4c166
adepegba1 <adepegbadavid@rocketmail.com>
2025-10-08 16:44:01 +0100

    Contact page done

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -7,3 +7,20 @@ document.getElementById(
 document.getElementById(
   "last-modified"
 ).textContent = `Last Modified ${document.lastModified}`;
+
+subjects = {
+  sql: "SQL",
+  excel: "Mircosoft Excel",
+  powerBi: "Power Bi",
+  python: "Python",
+};
+
+const subjectList = document.getElementById("subject");
+if (subjectList) {
+  for (const [key, value] of Object.entries(subjects)) {
+    const option = document.createElement("option");
+    option.value = key;
+    option.textContent = value;
+    subjectList.appendChild(option);
+  }
+}
